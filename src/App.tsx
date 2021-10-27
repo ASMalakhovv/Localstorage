73c09b62d21087ff6b3990da353107324ef99096
ASMalakhovv <alsmalahovv@mail.ru>
2021-10-27 19:03:43 +0600

    added localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [value,setValue] = useState<number>(0)
+  const incHandler = () => {
+    setValue(value+1)
+  }
+  const setToLocalStorageHandler = () => {
+
+  }
+
+  const getFromLocalStorageHandler = () => {
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1>{value}</h1>
+        <button onClick={incHandler}>inc</button>
+        <button onClick={setToLocalStorageHandler}>setToLocalStorage</button>
+        <button onClick={getFromLocalStorageHandler}>getFromLocalStorage</button>
+
+      </div>
   );
 }
 
