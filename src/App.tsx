8f53548b0d72e05a3a05dd682cf14fa5b20d92d6
ASMalakhovv <alsmalahovv@mail.ru>
2021-10-27 19:29:58 +0600

    added clickHandler

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [value,setValue] = useState<number>(0)
-  const incHandler = () => {
-    setValue(value+1)
-  }
-  const setToLocalStorageHandler = () => {
+    const [value, setValue] = useState<number>(0)
+    const incHandler = () => {
+        setValue(value + 1)
+    }
+    const setToLocalStorageHandler = () => {
+        localStorage.setItem("save value", JSON.stringify(value))
+    }
 
-  }
+    const getFromLocalStorageHandler = () => {
+       let valueAsString = localStorage.getItem("save value")
+        if (valueAsString) {
+            let getValue = JSON.parse(valueAsString)
+            setValue(getValue)
+        }
+    }
 
-  const getFromLocalStorageHandler = () => {
+    const clearLocalStorageHandler = () => {
+        localStorage.clear()
+        setValue(0)
+    }
+    return (
+        <div className="App">
+            <h1>{value}</h1>
+            <button onClick={incHandler}>inc</button>
+            <button onClick={setToLocalStorageHandler}>setToLocalStorage</button>
+            <button onClick={getFromLocalStorageHandler}>getFromLocalStorage</button>
+            <button onClick={clearLocalStorageHandler}>clearLocalStorage</button>
 
-  }
-  return (
-      <div className="App">
-        <h1>{value}</h1>
-        <button onClick={incHandler}>inc</button>
-        <button onClick={setToLocalStorageHandler}>setToLocalStorage</button>
-        <button onClick={getFromLocalStorageHandler}>getFromLocalStorage</button>
-
-      </div>
-  );
+        </div>
+    );
 }
 
 export default App;
